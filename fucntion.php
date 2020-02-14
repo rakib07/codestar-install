9f1b3890a71bf67ffedd28bf123ab89b5ddeb2ec
DM Rakib Hossain <rakib07@users.noreply.github.com>
2020-02-15 01:35:32 +0600

    Create fucntion.php

---
+++ fucntion.php
@@ -0,0 +1,63 @@
+Quick Start
+Download the installable WordPress plugin zip.
+Upload and activate plugin from WordPress → Plugins → Add New
+Open your current theme functions.php file and paste this code.
+
+
+// Control core classes for avoid errors
+if( class_exists( 'CSF' ) ) {
+
+  //
+  // Set a unique slug-like ID
+  $prefix = 'my_framework';
+
+  //
+  // Create options
+  CSF::createOptions( $prefix, array(
+    'menu_title' => 'My Framework',
+    'menu_slug'  => 'my-framework',
+  ) );
+
+  //
+  // Create a section
+  CSF::createSection( $prefix, array(
+    'title'  => 'Tab Title 1',
+    'fields' => array(
+
+      //
+      // A text field
+      array(
+        'id'    => 'opt-text',
+        'type'  => 'text',
+        'title' => 'Simple Text',
+      ),
+
+    )
+  ) );
+
+  //
+  // Create a section
+  CSF::createSection( $prefix, array(
+    'title'  => 'Tab Title 2',
+    'fields' => array(
+
+      // A textarea field
+      array(
+        'id'    => 'opt-textarea',
+        'type'  => 'textarea',
+        'title' => 'Simple Textarea',
+      ),
+
+    )
+  ) );
+
+}
+Click to copy
+How to get option value ?
+//
+// Get options
+$options = get_option( 'my_framework' ); // unique id of the framework
+
+echo $options['opt-text']; // id of the field
+echo $options['opt-textarea']; // id of the field
+Click to copy
